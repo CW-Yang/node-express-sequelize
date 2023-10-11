886d24e2fa112ce444d288309845b60195bc447c
Chi-Wei Yang <phillisping@gmail.com>
2023-10-11 10:45:58 +0800

    add: update product data method

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,8 +35,51 @@ exports.getProducts = (req, res, next) => {
       res.render('admin/products', {
         products,
         pageTitle: 'Admin Products',
-        path: '/admin/products'
+        path: '/admin/products',
+        isAuthenticated: req.session.isLogin
       });
     })
     .catch(e => console.log(err));
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const id = req.params.productId;
+  const isEdit = req.query.edit;
+  
+  Product.findByPk(id)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/admin/products');
+      }
+
+      res.render('admin/edit-product', {
+        product,
+        pageTitle: 'Edit Product',
+        path: '/admin/products',
+        isEdit,
+        isAuthenticated: req.session.isLogin 
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const id = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  Product.findByPk(id)
+    .then(product => {
+      product.title = updatedTitle;
+      product.imageUrl = updatedImageUrl;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      return product.save(); 
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT');
+      return res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
